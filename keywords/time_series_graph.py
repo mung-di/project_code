1055da7d78cc41010a9c13732b306ce4ac162dd5
mung-di <iso.o2i33@gmail.com>
2021-09-28 16:53:37 +0900

    time series graph_test

--- keywords/time_series_graph.py
+++ keywords/time_series_graph.py
@@ -11,10 +11,22 @@ connection = pymysql.connect(host='192.168.0.37',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 except_count = 0
+count=0
 last_date  = date.today()
 word_list = ""
 
+
 fig = go.Figure()
+
+def draw_graph():
+    word = word_list.split(',')
+    my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:15]
+    my_dict.reverse()  # 그래프 값이 큰 순서대로 넣기
+    x, y = zip(*my_dict)
+
+    fig = go.Figure(go.Bar(x=y, y=x, orientation='h'))
+    graph = fig.to_json()
+    return graph
 
 try:
     with connection.cursor() as cursor:
@@ -30,37 +42,23 @@ try:
             words = row['data']
             subdate = row['subdate']
 
-            if final_data == row:
-                word = word_list.split(',')
+            if subdate.isocalendar()[1] >= final_data['subdate'].isocalendar()[1] - 4 : # 최근 4주치 그래프만 나타내기 위해
 
+                if final_data == row: #이번주차
+                    draw_graph()
+                else: #이번주 제외 3주
+                    if last_date.isocalendar()[1] == subdate.isocalendar()[1]:
+                        word_list += words
 
-
-            else:
-                if last_date.isocalendar()[1] == subdate.isocalendar()[1]:
-                    word_list += words
-
-                elif last_date.isocalendar()[1] != subdate.isocalendar()[1]:
-                    if word_list != "":
-                        word = word_list.split(',')
-
-                        my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:15]
-                        my_dict.reverse() #그래프 값이 큰 순서대로 넣기
-                        x,y = zip(*my_dict)
-
-                        fig = go.Figure(go.Bar(x=y, y=x, orientation='h'))
-
-                        fig.show()
-                        fig.to_json()
-
+                    elif last_date.isocalendar()[1] != subdate.isocalendar()[1]:
+                        if word_list != "":
+                            draw_graph()
 
 
                     word_list = ""
                     word_list += words
 
-                    # print(subdate.month,word_list)
-
-
-                last_date = subdate
+            last_date = subdate
 
 
 finally:
