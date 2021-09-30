1a13fb355d55ac412736b5b4c6fef73001881d1b
mung-di <iso.o2i33@gmail.com>
2021-09-30 16:44:59 +0900

    리스트형식으로 넣기

--- keywords/time_series_graph.py
+++ keywords/time_series_graph.py
@@ -11,7 +11,7 @@ connection = pymysql.connect(host='192.168.0.37',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 except_count = 0
-count=0
+graph_list =[]
 last_date  = date.today()
 word_list = ""
 
@@ -45,7 +45,9 @@ try:
             if subdate.isocalendar()[1] >= final_data['subdate'].isocalendar()[1] - 4 : # 최근 4주치 그래프만 나타내기 위해
 
                 if final_data == row: #이번주차
-                    draw_graph()
+                    graph_list.append(draw_graph())
+
+
 
                 else: #이번주 제외 3주
                     if last_date.isocalendar()[1] == subdate.isocalendar()[1]:
@@ -53,14 +55,15 @@ try:
 
                     elif last_date.isocalendar()[1] != subdate.isocalendar()[1]:
                         if word_list != "":
-                            draw_graph()
+                            graph_list.append(draw_graph())
+
 
 
                     word_list = ""
                     word_list += words
 
             last_date = subdate
-
+        print(graph_list)
 
 finally:
     connection.close()
