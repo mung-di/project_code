83e3dbeb4423f1724eab3a85153062e61fda6a35
mung-di <iso.o2i33@gmail.com>
2021-10-02 10:24:51 +0900

    top10 2달치만 보여주기

--- keywords/top_10.py
+++ keywords/top_10.py
@@ -11,7 +11,8 @@ connection = pymysql.connect(host='192.168.0.37',
 except_count = 0
 last_date = date.today()
 word_list = ""
-top =[]
+this_top =[]
+last_top =[]
 try:
     with connection.cursor() as cursor:
         sql = "select subdate,data from daydata"
@@ -26,29 +27,31 @@ try:
             words = row['data']
             subdate = row['subdate']
 
-            if final_data == row:
-                word = word_list.split(',')
+            if subdate.month >= final_data['subdate'].month - 1:  # 최근 2달치 그래프만 나타내기 위해
 
-                print(last_date.month, Counter(word))
-                my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:10]
-                print(my_dict)
-                for k in range(10):
-                    top.append(my_dict[k][0])
-                print(top)
+                if final_data == row:
+                    word = word_list.split(',')
 
-            else:
-                if last_date.month == subdate.month:
-                    word_list += words
-                elif last_date.month != subdate.month:
-                    if word_list != "":
-                        word = word_list.split(',')
+                    # print(last_date.month, Counter(word))
+                    my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:10]
+                    # print(my_dict)
+                    for k in range(10):
+                        this_top.append(my_dict[k][0])
+                    print(this_top)
 
-                        print(last_date.month, Counter(word))
-                        my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:10]
-                        for k in range(10) :
-                            top.append(my_dict[k][0])
-                        print(top)
-                        top=[]
+                else:
+                    if last_date.month == subdate.month:
+                        word_list += words
+                    elif last_date.month != subdate.month:
+                        if word_list != "":
+                            word = word_list.split(',')
+
+                            # print(last_date.month, Counter(word))
+                            my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:10]
+                            for k in range(10) :
+                                last_top.append(my_dict[k][0])
+                            print(last_top)
+
 
 
 
