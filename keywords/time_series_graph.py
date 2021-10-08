a7ecb9c03eb71ebddf8f0f5aeb5d6fa00114389a
mung-di <iso.o2i33@gmail.com>
2021-10-08 10:10:07 +0900

    test

--- keywords/time_series_graph.py
+++ keywords/time_series_graph.py
@@ -22,7 +22,7 @@ fig = go.Figure()
 def draw_graph():
     word = word_list.split(',')
     my_dict = sorted(Counter(word).items(), key=lambda x: x[1], reverse=True)[:15]
-    my_dict.reverse()  # 그래프 값이 큰 순서대로 넣기위해 reverse , 하지않으면 상단에 작은 순서대로 출력
+    my_dict.reverse()  # 그래프 값이 큰 순서대로 넣기위해 reverse
     x, y = zip(*my_dict)
 
     fig = go.Figure(go.Bar(x=y, y=x, orientation='h'))
@@ -42,11 +42,11 @@ try:
         for row in rows:
             words = row['data']
             subdate = row['subdate']
+            # 최근 4주치 그래프만 나타내기 위해
+            if subdate.isocalendar()[1] >= final_data['subdate'].isocalendar()[1] - 4 :
 
-            if subdate.isocalendar()[1] >= final_data['subdate'].isocalendar()[1] - 4 : # 최근 4주치 그래프만 나타내기 위해
-
-                if final_data == row: #이번주차
-                    graph_list.append('graph:'+draw_graph()) # 구분자 추가
+                if final_data.isocalendar()[1] == row.isocalendar()[1]: #이번주차
+                    graph_list.append(draw_graph())
 
                 else: #이번주 제외 3주
                     if last_date.isocalendar()[1] == subdate.isocalendar()[1]:
@@ -54,7 +54,7 @@ try:
 
                     elif last_date.isocalendar()[1] != subdate.isocalendar()[1]:
                         if word_list != "":
-                            graph_list.append('graph:'+draw_graph()) # 구분자 추가 ex)split시 ,graph:로 하면 될 것 같음!!
+                            graph_list.append(draw_graph())
 
 
 
